cef598cba6b779dc6291ede6cb3c0360c88896e2
Trevor Kelly <Trevor.daniel.kelly@gmail.com>
2022-02-04 17:18:54 -0500

    monkey paws press the letters now

--- public/javascripts/modules/monkey.mjs
+++ public/javascripts/modules/monkey.mjs
@@ -1,7 +1,9 @@
 import DOMKeyboard from "dom-keyboard";
 import Templates from "../utils/templates.mjs";
 
-const DB_LOCATION = "http://localhost:3001/"
+const DB_LOCATION = "http://localhost:3001/";
+const LEFT_PAW = "../../images/monkey_hand_left.png";
+const RIGHT_PAW = "../../images/monkey_hand_right.png";
 
 function Monkey(name, node) {
   this.name = name;
@@ -24,13 +26,18 @@ function Monkey(name, node) {
 Monkey.prototype = {
   constructor: Monkey,
 
-  hitKey() {
+  async hitKey() {
     const letter = randomLetter();
-    this.keyboard.press(letter);
+    const key = this.keyboard.getKey(letter);
+    await pawPressLetter.call(this, key);
+    key.press();
     this.output.innerHTML += letter;
+    await pawReset.call(this);
     this.fullData.presses += 1;
     this.sendData.presses += 1;
-    return letter;
+    this.checkHamlet(letter);
+    this.stats.innerHTML = Templates.stats(this);
+    if (this.sendData.presses > 100) this.send();
   },
 
   checkHamlet(letter) {
@@ -59,18 +66,9 @@ Monkey.prototype = {
     this.sendData.hits[hamletSlice] += 1;
   },
 
-  run() {
-    let letter = this.hitKey();
-    this.statCheck = setInterval(async () => {
-      this.checkHamlet(letter);
-      letter = this.hitKey();
-      this.stats.innerHTML = Templates.stats(this);
-      if (this.sendData.presses > 100) this.send();
-    }, 1000);
-  },
-
-  stop() {
-    clearInterval(this.statCheck);
+  async run() {
+    await this.hitKey();
+    this.run();
   },
 
   send() {
@@ -101,14 +99,61 @@ function isHamlet(letter) {
 
 function buildMonkeyNode(node, name) {
   this.node = node;
-  this.keyboard = new DOMKeyboard("95%", `${name}-keyboard`);
+  this.keyboard = new DOMKeyboard("100%", `${name}-keyboard`);
 
   this.node.innerHTML = Templates.monkey(this);
   this.output = this.node.getElementsByClassName("output")[0];
   this.stats = this.node.getElementsByClassName("stats")[0];
 
+  this.paws = buildPaws.call(this);
+
   this.node.appendChild(this.keyboard.node);
   this.node.classList.add("monkey");
+}
+
+function buildPaws() {
+  const left = document.createElement("img");
+  const right = document.createElement("img");
+
+  left.src = LEFT_PAW;
+  left.alt = "Left Paw";
+  right.src = RIGHT_PAW;
+  right.alt = "Right Paw";
+
+  left.classList.add("paw", "left-paw");
+  right.classList.add("paw", "right-paw");
+
+  this.node.appendChild(left)
+  this.node.appendChild(right)
+
+  return { left, right }
+}
+
+function pawPressLetter(key) {
+  const promise = new Promise((resolve, reject) => {
+    if (key.side === "Left") {
+      const paw = this.paws.left;
+      paw.style.top = `${key.node.offsetTop + (parseInt(paw.height) / 2) + key.node.offsetHeight}px`;
+      paw.style.left = `${key.node.offsetLeft - (parseInt(paw.width) / 2) - key.node.offsetWidth}px`
+    } else {
+      const paw = this.paws.right;
+      paw.style.top = `${key.node.offsetTop + (parseInt(paw.height) / 2) + key.node.offsetHeight}px`;
+      paw.style.left = `${key.node.offsetLeft}px`
+    }
+    setTimeout( () => resolve(), 500);
+  });
+  return promise;
+}
+
+function pawReset() {
+  const promise = new Promise((resolve, reject) => {
+    this.paws.left.style.left = null;
+    this.paws.left.style.top = null;
+    this.paws.right.style.left = null;
+    this.paws.right.style.top = null;
+    setTimeout( () => resolve(), 500);
+  });
+  return promise;
 }
 
 const HAMLET = "FRANCISCO at his post. Enter to him BERNARDO";
